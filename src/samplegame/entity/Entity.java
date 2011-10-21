2699590e67397090743f0d025f5df9124d2c31fa
efruchter <fruchter.eric@gmail.com>
2011-10-21 13:19:00 -0400

    Added entity template

---
+++ src/samplegame/entity/Entity.java
@@ -0,0 +1,28 @@
+package samplegame.entity;
+
+/**
+ * A scripted entity that exists in the game world.
+ * 
+ * @author toriscope
+ * 
+ */
+public class Entity {
+
+	/**
+	 * Holds all variables for the entity.
+	 */
+	private VariableCase variables = new VariableCase();
+
+	/**
+	 * This variable case will be passed in containing the additional data from
+	 * the xml level file, as well as entity data from the entity xml.
+	 * 
+	 * @param variables
+	 */
+	public Entity(final VariableCase variables) {
+		this.variables = variables;
+	}
+}
+/*
+ * TODO: Add scripting support.
+ */
