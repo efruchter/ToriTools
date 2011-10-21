ff81982df080459ea9ecc8a7f19f6709cf88480f
efruchter <fruchter.eric@gmail.com>
2011-10-21 01:10:45 -0400

    Added lwjgl and outlined package for samplegame

---
+++ src/samplegame/SampleGame.java
@@ -0,0 +1,14 @@
+package samplegame;
+
+/**
+ * This will be the main class for a simple game that uses toritools.
+ * 
+ * @author toriscope
+ * 
+ */
+public class SampleGame {
+
+	public static void main(final String[] args) {
+		System.out.println("A fun game!");
+	}
+}
