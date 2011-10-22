8f570783506b24239f3bcadd3d9f1aadd37ed751
efruchter <fruchter.eric@gmail.com>
2011-10-22 18:41:49 -0400

    Rhino scripting library

--- src/samplegame/entrypoint/SampleGame.java
+++ src/samplegame/entrypoint/SampleGame.java
@@ -2,8 +2,6 @@ package samplegame.entrypoint;
 
 import java.io.FileNotFoundException;
 
-import samplegame.scripting.ScriptFactory;
-
 /**
  * This will be the main class for a simple game that uses toritools.
  * 
@@ -12,8 +10,7 @@ import samplegame.scripting.ScriptFactory;
  */
 public class SampleGame {
 
-	public static void main(final String[] args) throws FileNotFoundException {
-		ScriptFactory s = new ScriptFactory();
-		s.create().onSpawn();
-	}
+    public static void main(final String[] args) throws FileNotFoundException {
+
+    }
 }
---
+++ src/samplegame/scripting/ScriptFactoryJS.java
@@ -0,0 +1,7 @@
+package samplegame.scripting;
+
+public class ScriptFactoryJS {
+    public static void main(String[] args) {
+
+    }
+}
