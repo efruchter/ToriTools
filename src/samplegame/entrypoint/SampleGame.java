aab09c0d2b8b72040bbc53c9762f2d46b3cf844c
efruchter <fruchter.eric@gmail.com>
2011-10-22 19:31:59 -0400

    LWJGL Rendering

--- src/samplegame/entrypoint/SampleGame.java
+++ src/samplegame/entrypoint/SampleGame.java
@@ -2,6 +2,17 @@ package samplegame.entrypoint;
 
 import java.io.FileNotFoundException;
 
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.Color;
+import org.lwjgl.util.glu.GLU;
+import org.lwjgl.util.vector.Vector2f;
+
+import samplegame.render.Render;
+
 /**
  * This will be the main class for a simple game that uses toritools.
  * 
@@ -10,7 +21,139 @@ import java.io.FileNotFoundException;
  */
 public class SampleGame {
 
-    public static void main(final String[] args) throws FileNotFoundException {
+    /** Game title */
+    public static final String GAME_TITLE = "SampleGame";
 
+    /** Exit the game */
+    private static boolean finished;
+
+    /** Desired frame time */
+    private static final int FRAMERATE = 60;
+
+    public static final Vector2f BOUNDS = new Vector2f(800, 600);
+
+    /**
+     * Application init
+     * 
+     * @param args
+     *            Commandline args
+     */
+    public static void main(String[] args) {
+        try {
+            init();
+            run();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            Sys.alert(GAME_TITLE, "An error occured and the game will exit.");
+        } finally {
+            cleanup();
+        }
+        System.exit(0);
     }
+
+    /**
+     * Initialize the game
+     * 
+     * @throws Exception
+     *             if init fails
+     */
+    private static void init() throws Exception {
+        /**
+         * Display windows stuff.
+         */
+        Display.setTitle(GAME_TITLE);
+        Display.setVSyncEnabled(true);
+        Display.setDisplayMode(new DisplayMode((int) BOUNDS.getX(),
+                (int) BOUNDS.getY()));
+        Display.create();
+    }
+
+    /**
+     * Runs the game (the "main loop")
+     */
+    private static void run() {
+        while (!finished) {
+            // Always call Window.update(), all the time - it does some behind
+            // the
+            // scenes work, and also displays the rendered output
+            Display.update();
+            // Check for close requests
+            if (Display.isCloseRequested()) {
+                finished = true;
+            }
+            // The window is in the foreground, so we should play the game
+            else if (Display.isActive()) {
+                logic();
+                render();
+                Display.sync(FRAMERATE);
+            }
+            // The window is not in the foreground, so we can allow other stuff
+            // to run and
+            // infrequently update
+            else {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+                logic();
+                // Only bother rendering if the window is visible or dirty
+                if (Display.isVisible() || Display.isDirty()) {
+                    render();
+                }
+            }
+        }
+    }
+
+    /**
+     * Do any game-specific cleanup
+     */
+    private static void cleanup() {
+        // Close the window
+        Display.destroy();
+    }
+
+    /**
+     * Do all calculations, handle input, etc.
+     */
+    private static void logic() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            finished = true;
+        }
+    }
+
+    /**
+     * Render the current frame
+     */
+    private static void render() {
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GLU.gluPerspective(45, BOUNDS.x / BOUNDS.y, 0, 10000);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+        // clear the screen and set the camera
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT);
+
+        GL11.glPushMatrix();
+        GL11.glTranslatef(-BOUNDS.x / 2, -BOUNDS.y / 2, -725);
+
+        Render.setColor(Color.BLACK, .6f);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex3f(0, 0, 0);
+        GL11.glVertex3f(BOUNDS.getX(), 0, 0);
+        GL11.glVertex3f(BOUNDS.getX(), BOUNDS.getY(), 0);
+        GL11.glVertex3f(0, BOUNDS.getY(), 0);
+        GL11.glEnd();
+
+        /**
+         * DRAW ALL THE THINGS
+         */
+        Render.setColor(Color.GREEN);
+        Render.fillRect(new Vector2f(20, 20), new Vector2f(20, 20));
+
+        GL11.glPopMatrix();
+    }
+
 }
--- src/samplegame/render/Render.java
+++ src/samplegame/render/Render.java
@@ -14,71 +14,84 @@ import org.lwjgl.util.vector.Vector2f;
  */
 public class Render {
 
-	private Render() {
-	}
+    private Render() {
+    }
 
-	/**
-	 * Push a matrix Triangle fan in the shape of a circle. Does not set color
-	 * or fill style.
-	 */
-	public static void fillCircle(final Vector2f center, final float radius) {
+    /**
+     * Push a matrix Triangle fan in the shape of a circle. Does not set color
+     * or fill style.
+     */
+    public static void fillCircle(final Vector2f center, final float radius) {
 
-		final float FULL_CIRCLE = (float) Math.PI * 2, SEGMENTS = 20;
-		GL11.glPushMatrix();
-		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
-		for (float angle = 0; angle < FULL_CIRCLE; angle += FULL_CIRCLE
-				/ SEGMENTS) {
-			Vector2f newPoint = Vector2f.add(center,
-					new Vector2f(radius * (float) Math.cos(angle), radius
-							* (float) Math.sin(angle)), null);
-			GL11.glVertex2f(newPoint.getX(), newPoint.getY());
-		}
-		GL11.glEnd();
-		GL11.glPopMatrix();
-	}
+        final float FULL_CIRCLE = (float) Math.PI * 2, SEGMENTS = 20;
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+        for (float angle = 0; angle < FULL_CIRCLE; angle += FULL_CIRCLE
+                / SEGMENTS) {
+            Vector2f newPoint = Vector2f.add(center,
+                    new Vector2f(radius * (float) Math.cos(angle), radius
+                            * (float) Math.sin(angle)), null);
+            GL11.glVertex2f(newPoint.getX(), newPoint.getY());
+        }
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
 
-	/**
-	 * Push a matrix polygon. Does not set color or fill style.
-	 */
-	public static void drawPoly(final Vector2f... points) {
-		GL11.glPushMatrix();
-		GL11.glBegin(GL11.GL_LINES);
-		for (Vector2f newPoint : points)
-			GL11.glVertex2f(newPoint.getX(), newPoint.getY());
-		GL11.glEnd();
-		GL11.glPopMatrix();
-	}
+    /**
+     * Push a matrix polygon.
+     */
+    public static void drawPoly(final Vector2f... points) {
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_LINES);
+        for (Vector2f newPoint : points)
+            GL11.glVertex2f(newPoint.getX(), newPoint.getY());
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
 
-	public static void drawRect(final Vector2f pos, final Vector2f dim) {
-		drawPoly(pos, new Vector2f(pos.x + dim.x, pos.y),
-				Vector2f.add(pos, dim, null),
-				new Vector2f(pos.x, pos.y + dim.y), pos);
-	}
+    /**
+     * Push a matrix polygon.
+     */
+    public static void fillPoly(final Vector2f... points) {
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_QUADS);
+        for (Vector2f newPoint : points)
+            GL11.glVertex2f(newPoint.getX(), newPoint.getY());
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
 
-	/**
-	 * Set the GL Color and alpha
-	 */
-	public static void setColor(final Color c, float alpha) {
-		GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), alpha);
-	}
+    public static void fillRect(final Vector2f pos, final Vector2f dim) {
 
-	/**
-	 * Set the GL Color. Alpha defaults to 1.
-	 */
-	public static void setColor(final Color c) {
-		GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), 1f);
-	}
+        fillPoly(pos, new Vector2f(pos.x + dim.x, pos.y),
+                Vector2f.add(pos, dim, null),
+                new Vector2f(pos.x, pos.y + dim.y), pos);
+    }
 
-	public static void setColor(final ReadableColor c) {
-		GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), 1f);
-	}
+    /**
+     * Set the GL Color and alpha
+     */
+    public static void setColor(final Color c, float alpha) {
+        GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+    }
 
-	public static void setColor(final ReadableColor c, final float alpha) {
-		GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), alpha);
-	}
+    /**
+     * Set the GL Color. Alpha defaults to 1.
+     */
+    public static void setColor(final Color c) {
+        GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), 1f);
+    }
 
-	public static void setFill(final boolean fill) {
-		GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, fill ? GL11.GL_FILL
-				: GL11.GL_LINE);
-	}
+    public static void setColor(final ReadableColor c) {
+        GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), 1f);
+    }
+
+    public static void setColor(final ReadableColor c, final float alpha) {
+        GL11.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+    }
+
+    public static void setFill(final boolean fill) {
+        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, fill ? GL11.GL_FILL
+                : GL11.GL_LINE);
+    }
 }
