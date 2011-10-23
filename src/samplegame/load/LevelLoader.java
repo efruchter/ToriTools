9ca346268061e6db33056914520f906b685602ff
efruchter <fruchter.eric@gmail.com>
2011-10-22 20:28:30 -0400

    Added  print

--- src/samplegame/load/LevelLoader.java
+++ src/samplegame/load/LevelLoader.java
@@ -2,9 +2,12 @@ package samplegame.load;
 
 /**
  * Loa d a level and import all entities.
+ * 
  * @author toriscope
- *
+ * 
  */
 public class LevelLoader {
-    public 
+    public static void main(String[] args) {
+        System.out.println("YO!");
+    }
 }
