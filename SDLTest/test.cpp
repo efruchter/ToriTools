512981db066bd178038a166643131498ce827649
efruchter <fruchter.eric@gmail.com>
2011-12-18 01:58:33 -0500

    more sdl goofing around

--- SDLTest/test.cpp
+++ SDLTest/test.cpp
@@ -1,29 +1,73 @@
 #include "SDL/SDL.h"
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+SDL_Surface* load_image ( std::string filename )
+{
+    SDL_Surface* loadedImage = NULL;
+    SDL_Surface* optimizedImage = NULL;
+
+    loadedImage = SDL_LoadBMP( filename.c_str() );
+    if( loadedImage != NULL )
+    {
+        optimizedImage = SDL_DisplayFormat( loadedImage );
+
+        SDL_FreeSurface( loadedImage );
+    }
+
+    return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
+{
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+
+    SDL_BlitSurface( source, NULL, destination, &offset );
+}
 
 int main( int argc, char* args[] )
 {
     //The images
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+    SDL_Surface *message = NULL;
+    SDL_Surface *background = NULL;
+    SDL_Surface *screen = NULL;
 
     //Start DSL
-    SDL_Init( SDL_INIT_EVERYTHING );
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        return 1;
+    }
 
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+    if ( screen == NULL )
+    {
+        return 1;
+    }
 
-    hello = SDL_LoadBMP( "hello.bmp" );
+    SDL_WM_SetCaption( "Tori test blah" NULL );
 
-    //Apply image to screen
-    SDL_BlitSurface( hello, NULL, screen, NULL );
+    message = load_image( "cham.bmp" );
+    background = load_image( "bg.bmp" );
 
-    SDL_Flip( screen );
+    apply_surface( 0, 0, background, screen );
 
-    SDL_Delay( 2000 );
+    //Update the screen
+    if( SDL_Flip( screen ) == -1 )
+    {
+        return 1;    
+    }
 
-    SDL_FreeSurface( hello );
+    SDL_Delay ( 5000 );
 
-    //Quit SDL
-    SDL_Quit ();
+    SDL_FreeSurface( message );
+    SDL_FreeSurface( background );
+
+    SDL_Quit();
 
     return 0;
 }
