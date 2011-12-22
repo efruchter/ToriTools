bc066e25ad1c9525a02dce9ac49730ac67114de0
efruchter <fruchter.eric@gmail.com>
2011-12-22 01:24:10 -0500

    More refactoring

--- src/spaceFlight/SpaceFlight.java
+++ src/spaceFlight/SpaceFlight.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import spaceFlight.customScripts.BlastFactory;
 import toritools.controls.KeyHolder;
 import toritools.entity.Entity;
 import toritools.entity.Level;
--- src/spaceFlight/customScripts/BlastFactory.java
+++ src/spaceFlight/customScripts/BlastFactory.java
@@ -1,4 +1,4 @@
-package spaceFlight;
+package spaceFlight.customScripts;
 
 import java.awt.Color;
 import java.awt.Graphics;
