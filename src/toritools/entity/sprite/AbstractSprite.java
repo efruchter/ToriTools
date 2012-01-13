3da89d5290051a4be9d8c92b8863f30934c76b80
efruchter <fruchter.eric@gmail.com>
2012-01-12 21:47:27 -0500

    Better javadoc

--- src/toritools/entity/sprite/AbstractSprite.java
+++ src/toritools/entity/sprite/AbstractSprite.java
@@ -7,10 +7,22 @@ import java.awt.Image;
 import toritools.entity.Entity;
 import toritools.math.Vector2;
 
+/**
+ * This represents the visual component of an entity.
+ * 
+ * @author toriscope
+ * 
+ */
 public interface AbstractSprite {
 
+	/**
+	 * Advance the animation by one frame.
+	 */
 	void nextFrame();
 
+	/**
+	 * Set the current frame independent of the timestretch.
+	 */
 	void nextFrameAbsolute();
 
 	void setFrame(final int frame);
@@ -19,8 +31,18 @@ public interface AbstractSprite {
 
 	void set(final int frame, final int cycle);
 
+	/**
+	 * Stretch the animation by a factor.
+	 * 
+	 * @param timeStretch
+	 */
 	void setTimeStretch(final int timeStretch);
 
+	/**
+	 * Increase the dimensions of the sprite by a size.
+	 * 
+	 * @param sizeOffset
+	 */
 	void setsizeOffset(final int sizeOffset);
 
 	/**
