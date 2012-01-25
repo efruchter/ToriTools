099dcb9654ae38c6de2187fda8664a019f7c5e39
efruchter <fruchter.eric@gmail.com>
2012-01-25 16:25:26 -0500

    Added blur

--- src/audioProject/entities/BadShip.java
+++ src/audioProject/entities/BadShip.java
@@ -2,6 +2,8 @@ package audioProject.entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.LinkedList;
+import java.util.List;
 
 import toritools.entity.Entity;
 import toritools.entity.Level;
@@ -13,33 +15,33 @@ import toritools.scripting.ScriptUtils;
 public class BadShip extends Entity {
 
 	public BadShip(final Vector2 startingPosition) {
-		
+
 		variables.setVar("health", "40");
 		type = "enemy";
-		
+
 		pos = startingPosition;
 		dim = new Vector2(30, 30);
 
 		addScript(new EntityScriptAdapter() {
 
 			float speed = .2f;
-			
-			//Entity player;
-			Level level;
 
+			// Entity player;
+			Level level;
 
 			@Override
 			public void onSpawn(Entity self) {
 				level = ScriptUtils.getCurrentLevel();
-				//player = level.getEntityWithId("player");
+				// player = level.getEntityWithId("player");
 			}
 
 			@Override
 			public void onUpdate(Entity self, float time) {
-				
+
 				self.setPos(self.getPos().add(-speed * time, 0));
-				
-				if(!ScriptUtils.isColliding(level, self) || variables.getFloat("health") <= 0) {
+
+				if (!ScriptUtils.isColliding(level, self)
+						|| variables.getFloat("health") <= 0) {
 					level.despawnEntity(self);
 				}
 			}
@@ -47,10 +49,26 @@ public class BadShip extends Entity {
 
 		setSprite(new AbstractSpriteAdapter() {
 
+			List<Vector2> pastPos = new LinkedList<Vector2>();
+			final int MAX_HISTORY = 3;
+
 			@Override
-			public void draw(Graphics g, Entity self, Vector2 position,	Vector2 dimension) {
-				g.setColor(Color.RED);
-				g.drawOval((int) position.x, (int) position.y, (int) dimension.x, (int) dimension.y);
+			public void draw(Graphics g, Entity self, Vector2 position,
+					Vector2 dimension) {
+
+				pastPos.add(0, position);
+
+				if (pastPos.size() > MAX_HISTORY) {
+					pastPos.remove(MAX_HISTORY);
+				}
+
+				int alpha = 255;
+				for (Vector2 hPos : pastPos) {
+					g.setColor(new Color(255, 0, 0, alpha));
+					g.fillOval((int) hPos.x, (int) hPos.y, (int) dimension.x,
+							(int) dimension.y);
+					alpha = alpha / 2;
+				}
 			}
 		});
 	}
--- src/audioProject/entities/GoodBullet.java
+++ src/audioProject/entities/GoodBullet.java
@@ -15,54 +15,61 @@ public class GoodBullet extends Entity {
 
 	public GoodBullet(final Vector2 position) {
 		type = "GoodBullet";
-		
+
 		layer = 1;
 
 		pos = position;
 		dim = new Vector2(10, 10);
 
 		addScript(new EntityScriptAdapter() {
-			
+
 			float damage = 5;
-			
+
 			Vector2 speed = new Vector2(.5f, 0);
-			
+
 			@Override
 			public void onUpdate(Entity self, float time) {
-				if (!ScriptUtils.isColliding(ScriptUtils.getCurrentLevel(), self)) {
+				if (!ScriptUtils.isColliding(ScriptUtils.getCurrentLevel(),
+						self)) {
 					ScriptUtils.getCurrentLevel().despawnEntity(self);
 				}
-				
+
 				self.setPos(self.getPos().add(speed.scale(time)));
-				
-				for (Entity enemy: ScriptUtils.getCurrentLevel().getEntitiesWithType("enemy")) {
-					if(ScriptUtils.isColliding(self, enemy)) {
+
+				for (Entity enemy : ScriptUtils.getCurrentLevel()
+						.getEntitiesWithType("enemy")) {
+					if (ScriptUtils.isColliding(self, enemy)) {
 						ScriptUtils.getCurrentLevel().despawnEntity(self);
-						enemy.getVariableCase().setVar("health", enemy.getVariableCase().getFloat("health") - damage + "");
+						enemy.getVariableCase().setVar(
+								"health",
+								enemy.getVariableCase().getFloat("health")
+										- damage + "");
 						break;
 					}
 				}
 			}
 		});
 
-setSprite(new AbstractSpriteAdapter() {
-			
+		setSprite(new AbstractSpriteAdapter() {
+
 			List<Vector2> pastPos = new LinkedList<Vector2>();
 			final int MAX_HISTORY = 3;
 
 			@Override
-			public void draw(Graphics g, Entity self, Vector2 position,	Vector2 dimension) {
-				
+			public void draw(Graphics g, Entity self, Vector2 position,
+					Vector2 dimension) {
+
 				pastPos.add(0, position);
-				
-				if(pastPos.size() > MAX_HISTORY) {
+
+				if (pastPos.size() > MAX_HISTORY) {
 					pastPos.remove(MAX_HISTORY);
 				}
-				
+
 				int alpha = 255;
-				for(Vector2 hPos: pastPos) {
+				for (Vector2 hPos : pastPos) {
 					g.setColor(new Color(0, 255, 0, alpha));
-					g.fillOval((int) hPos.x, (int) hPos.y, (int) dimension.x, (int) dimension.y);
+					g.fillOval((int) hPos.x, (int) hPos.y, (int) dimension.x,
+							(int) dimension.y);
 					alpha = alpha / 2;
 				}
 			}
