eff8d9c9ac302cfd4f7ff113aa7dd69da94a90c7
efruchter <fruchter.eric@gmail.com>
2012-03-07 15:59:52 -0500

    Set icon and test entity level

--- src/snakemeleon/Snakemeleon.java
+++ src/snakemeleon/Snakemeleon.java
@@ -2,10 +2,19 @@ package snakemeleon;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import javax.swing.ImageIcon;
+
+import toritools.entity.Entity;
 import toritools.entity.Level;
 import toritools.entrypoint.Binary;
+import toritools.io.Importer;
 import toritools.math.Vector2;
+import toritools.scripting.ScriptUtils;
 
 public class Snakemeleon extends Binary {
 
@@ -15,6 +24,7 @@ public class Snakemeleon extends Binary {
 
     public Snakemeleon() {
         super(new Vector2(800, 600), 60, "Snakemeleon");
+        super.getApplicationFrame().setIconImage(new ImageIcon("snakemeleon/chameleon_head.png").getImage());
     }
 
     @Override
@@ -31,9 +41,36 @@ public class Snakemeleon extends Binary {
 
     @Override
     protected boolean render(Graphics rootCanvas, Level level) {
-        Color c = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
-        rootCanvas.setColor(c);
-        rootCanvas.fillRect(0, 0, VIEWPORT.getWidth(), VIEWPORT.getHeight());
+
+        try {
+            ((Graphics2D) rootCanvas).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            Entity player = level.getEntityWithId("player");
+            if(player == null) {
+                System.out.println("You need to make an entity with id set to player!");
+                System.exit(1);
+            }
+            Vector2 playerPos = player.getPos();
+            Vector2 offset = VIEWPORT.scale(.5f).sub(playerPos);
+
+            rootCanvas.setColor(Color.BLACK);
+            rootCanvas.fillRect(0, 0, (int) VIEWPORT.x, (int) VIEWPORT.y);
+            for (int i = level.getLayers().size() - 1; i >= 0; i--) {
+                for (Entity e : level.getLayers().get(i)) {
+                    if (e.isVisible() && e.isInView())
+                        e.draw(rootCanvas, offset);
+                    if (!"BACKGROUND".equals(e.getType()) && ScriptUtils.isDebugMode()) {
+                        rootCanvas.setColor(Color.RED);
+                        rootCanvas.drawRect((int) (e.getPos().x + offset.x), (int) (e.getPos().y + offset.y),
+                                (int) e.getDim().x, (int) e.getDim().y);
+                    }
+
+                }
+            }
+
+        } catch (final Exception e) {
+            return false;
+        }
         return true;
     }
 
@@ -44,9 +81,13 @@ public class Snakemeleon extends Binary {
 
     @Override
     protected Level getStartingLevel() {
-        Level l = new Level();
-        l.setDim(VIEWPORT);
-        return l;
+        try {
+            return Importer.importLevel(new File("snakemeleon/TestLevel.xml"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
     }
 
 }
