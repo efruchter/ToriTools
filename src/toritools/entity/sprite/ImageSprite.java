de759a0297c1f6bdb46e9e4028619626b9442394
efruchter <fruchter.eric@gmail.com>
2012-03-07 19:23:36 -0500

    Fixed sprite size glitch.

--- src/toritools/entity/sprite/ImageSprite.java
+++ src/toritools/entity/sprite/ImageSprite.java
@@ -15,121 +15,115 @@ import toritools.math.Vector2;
 
 public class ImageSprite implements AbstractSprite {
 
-	private int xSplit = 1, ySplit = 1, x = 0, y = 0;// w, h;
-	private Image image;
+    private int xSplit = 1, ySplit = 1, x = 0, y = 0;// w, h;
+    private Image image;
 
-	private Vector2 bRight;
-	private int timeStretch = 1;
-	private float sizeOffset = 0;
+    private Vector2 bRight;
+    private int timeStretch = 1;
+    private float sizeOffset = 0;
 
-	/**
-	 * Create a sprite with given image;
-	 * 
-	 * @param image
-	 *            an image
-	 * @param xTiles
-	 *            tiles win x direction
-	 * @param yTiles
-	 *            tiles in y direction
-	 */
-	public ImageSprite(final Image image, final int xTiles, final int yTiles) {
-		this.image = image;
-		this.xSplit = xTiles;
-		this.ySplit = yTiles;
-		bRight = new Vector2(image.getWidth(null) / xSplit,
-				image.getHeight(null) / ySplit);
-	}
+    /**
+     * Create a sprite with given image;
+     * 
+     * @param image
+     *            an image
+     * @param xTiles
+     *            tiles win x direction
+     * @param yTiles
+     *            tiles in y direction
+     */
+    public ImageSprite(final Image image, final int xTiles, final int yTiles) {
+        this.image = image;
+        this.xSplit = xTiles;
+        this.ySplit = yTiles;
+        bRight = new Vector2(image.getWidth(null) / xSplit, image.getHeight(null) / ySplit);
+    }
 
-	/**
-	 * Use this constructor if you plan on overriding draw(); Everything will be
-	 * null.
-	 */
-	public ImageSprite() {
+    /**
+     * Use this constructor if you plan on overriding draw(); Everything will be
+     * null.
+     */
+    public ImageSprite() {
 
-	}
+    }
 
-	public void nextFrame() {
-		x = ++x % (xSplit * timeStretch);
-	}
+    public void nextFrame() {
+        x = ++x % (xSplit * timeStretch);
+    }
 
-	public void nextFrameAbsolute() {
-		int timeStretch = 1;
-		x = ++x % (xSplit * timeStretch);
-	}
+    public void nextFrameAbsolute() {
+        int timeStretch = 1;
+        x = ++x % (xSplit * timeStretch);
+    }
 
-	public void setFrame(final int frame) {
-		x = frame * timeStretch % (xSplit * timeStretch);
-	}
+    public void setFrame(final int frame) {
+        x = frame * timeStretch % (xSplit * timeStretch);
+    }
 
-	public void setCycle(final int cycle) {
-		y = cycle % ySplit;
-	}
+    public void setCycle(final int cycle) {
+        y = cycle % ySplit;
+    }
 
-	public void set(final int x, final int y) {
-		this.x = x;
-		this.y = y;
-	}
+    public void set(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	/**
-	 * Override this to implement your own drawing mechanism!
-	 * 
-	 * @param g
-	 * @param self
-	 * @param position
-	 * @param dimension
-	 */
-	public void draw(final Graphics g, final Entity self,
-			final Vector2 position, final Vector2 dimension) {
-		int x = this.x / timeStretch;
-		Vector2 dim = dimension.add(sizeOffset * 2);
-		Vector2 pos = position.sub(sizeOffset);
+    /**
+     * Override this to implement your own drawing mechanism!
+     * 
+     * @param g
+     * @param self
+     * @param position
+     * @param dimension
+     */
+    public void draw(final Graphics g, final Entity self, final Vector2 position, final Vector2 dimension) {
+        int x = this.x / timeStretch;
+        Vector2 dim = dimension.add(sizeOffset * 2);
+        Vector2 pos = position.sub(sizeOffset);
 
-		if (self.getDirection() != 0) {
-			
-			VolatileImage i = Binary.gc.createCompatibleVolatileImage(image.getWidth(null), image.getHeight(null), VolatileImage.TRANSLUCENT);
-			i.validate(Binary.gc);
+        if (self.getDirection() != 0) {
 
-			Graphics2D gr = (Graphics2D)i.getGraphics();
-			gr.setColor(new Color(0,0,0,0));
-			gr.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OUT));
-			gr.fillRect(0, 0, i.getWidth(), i.getHeight());
-			
-			gr.drawImage(image, (int) 0, (int) 0,
-					(int) dim.x, (int) dim.y, x * (int) bRight.x,
-					y * (int) bRight.y, x * (int) bRight.x + (int) bRight.x,
-					y * (int) bRight.y + (int) bRight.y, null);
+            VolatileImage i = Binary.gc.createCompatibleVolatileImage(dim.getWidth(), dim.getHeight(),
+                    VolatileImage.TRANSLUCENT);
+            i.validate(Binary.gc);
 
-			AffineTransform affineTransform = new AffineTransform();
-			// rotate with the anchor point as the mid of the image
-			affineTransform.translate(pos.x, pos.y);
-			affineTransform.rotate(Math.toRadians(self.getDirection()), dim.x / 2,
-					dim.y / 2);
+            Graphics2D gr = (Graphics2D) i.getGraphics();
+            gr.setColor(new Color(0, 0, 0, 0));
+            gr.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OUT));
+            gr.fillRect(0, 0, i.getWidth(), i.getHeight());
 
-			((Graphics2D) g).drawImage(i, affineTransform, null);
-		} else {
-			g.drawImage(image, (int) pos.x, (int) pos.y, (int) (pos.x + dim.x),
-					(int) (pos.y + dim.y), x * (int) bRight.x, y
-							* (int) bRight.y, x * (int) bRight.x
-							+ (int) bRight.x, y * (int) bRight.y
-							+ (int) bRight.y, null);
-		}
-	}
+            gr.drawImage(image, (int) 0, (int) 0, (int) dim.x, (int) dim.y, x * (int) bRight.x, y * (int) bRight.y, x
+                    * (int) bRight.x + (int) bRight.x, y * (int) bRight.y + (int) bRight.y, null);
 
-	public Image getImage() {
-		return image;
-	}
+            AffineTransform affineTransform = new AffineTransform();
+            // rotate with the anchor point as the mid of the image
+            affineTransform.translate(pos.x, pos.y);
+            affineTransform.rotate(Math.toRadians(self.getDirection()), dim.x / 2, dim.y / 2);
 
-	public Dimension getTileDimension() {
-		return new Dimension(x, y);
-	}
+            ((Graphics2D) g).drawImage(i, affineTransform, null);
+        } else {
+            g.drawImage(image, (int) pos.x, (int) pos.y, (int) (pos.x + dim.x), (int) (pos.y + dim.y), x
+                    * (int) bRight.x, y * (int) bRight.y, x * (int) bRight.x + (int) bRight.x, y * (int) bRight.y
+                    + (int) bRight.y, null);
+        }
+    }
 
-	@Override
-	public void setTimeStretch(int timeStretch) {
-		this.timeStretch = timeStretch;
-	}
+    public Image getImage() {
+        return image;
+    }
 
-	@Override
-	public void setsizeOffset(int sizeOffset) {
-		this.sizeOffset = sizeOffset;		
-	}
+    public Dimension getTileDimension() {
+        return new Dimension(x, y);
+    }
+
+    @Override
+    public void setTimeStretch(int timeStretch) {
+        this.timeStretch = timeStretch;
+    }
+
+    @Override
+    public void setsizeOffset(int sizeOffset) {
+        this.sizeOffset = sizeOffset;
+    }
 }
